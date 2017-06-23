f51e0779af59520442a8fe108d878c4bfba86ad7
Neto Barbosa <jbsn@cin.ufpe.br>
2017-06-23 13:05:08 -0300

    Instalação do docker e criação de imagem

--- executa.sh
+++ executa.sh
@@ -79,3 +79,36 @@ cd DEV
 mvn clean install
 
 mvn sonar:sonar
+
+sudo apt-get remove docker docker-engine
+
+sudo apt-get update
+
+sudo apt-get -y install \
+    linux-image-extra-$(uname -r) \
+    linux-image-extra-virtual
+
+sudo apt-get -y install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo apt-key fingerprint 0EBFCD88
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt-get update
+
+sudo apt-get -y install docker-ce
+
+sudo docker build -t if1006petclinics .
+
+sudo docker tag if1006petclinics jbsn/if1006petclinics
+
+sudo docker push jbsn/if1006petclinics
